8fc30e9f51089f67dedb7069558784a7d1b7571d
gokul21821 <gokul@vexalink.com>
2025-12-08 05:09:54 +0530

    migration for image

---
+++ src/prisma/migrations/20251207230146_add_image_path/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "Blog" ADD COLUMN     "imagePath" TEXT;
+
+-- AlterTable
+ALTER TABLE "CaseStudy" ADD COLUMN     "imagePath" TEXT;
+
+-- AlterTable
+ALTER TABLE "Newsletter" ADD COLUMN     "imagePath" TEXT;
